1a0c16b3a9dd75a4e9bcaef9e319e906cfb9fa5c
Juana Velasquez Lazo <jvelasquezl@ucenfotec.ac.cr>
2025-11-13 22:32:33 -0600

    Actualización del archivo
    Apellido

--- mi-archivo.js
+++ mi-archivo.js
@@ -2,7 +2,7 @@ public class HolaMundo {
 
     public static void main(String[] args) { 
         System.out.println("¡Hola, mundo desde Java!"); 
-        saludar("Juana"); 
+        saludar("Juana Velasquez"); 
         sumar(5, 3); 
     }
 
